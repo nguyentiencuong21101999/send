1332a135e807a354c418b52ce4a5cf948fa58967
Nguyễn Tiến Cường <cuong.nguyen@apps-cyclone.com>
2022-04-19 11:04:40 +0700

    Add notification

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import "./App.css";
-
+import React, { useState, useEffect, useCallback } from "react";
 import { SendBirdProvider as SBProvider } from "sendbird-uikit";
 import { APP_ID, USER_ID, NICKNAME } from "./config";
 import CustomizedApp from "./CustomizedApp";
 import "sendbird-uikit/dist/index.css";
 
+import firebase from "./service/firebaseInit";
+import "@firebase/messaging";
+
 function App() {
+  const [token, setToken] = useState(null);
+  React.useEffect(() => {
+    const msg = firebase.messaging();
+    msg
+      .getToken()
+      .then((data) => setToken(data))
+      .catch((err) => console.log(err));
+    console.log();
+    // msg
+    //   .requestPermission()
+    //   .then(() => {
+    //     return msg.getToken();
+    //   })
+    //   .then((data) => {
+    //     console.warn("token", data);
+    //     setToken(data);
+    //   });
+  });
   return (
     <div className="app-wrapper">
-      <SBProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME}>
+      Token: {token}
+      {/* <SBProvider appId={APP_ID} userId={USER_ID} nickname={NICKNAME}>
         <CustomizedApp />
-      </SBProvider>
+      </SBProvider> */}
     </div>
   );
 }
---
+++ src/service/firebaseInit.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAgVT7d1xeWbA0Gk0dPQeiCsiyl1bXpjU",
+  authDomain: "web-push-5fc9e.firebaseapp.com",
+  projectId: "web-push-5fc9e",
+  storageBucket: "web-push-5fc9e.appspot.com",
+  messagingSenderId: "258402861035",
+  appId: "1:258402861035:web:266919e1cb7f816093e3ae",
+  measurementId: "G-H2WC8FCZCV",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
